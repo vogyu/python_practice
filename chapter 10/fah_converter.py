38f1d994758eb6062c4923964b3383f9537b086d
unknown <hb6202@naver.com>
2022-06-15 03:13:42 +0900

    first commit

---
+++ chapter 10/fah_converter.py
@@ -0,0 +1,2 @@
+def converter_c_to_f(celcius_value):
+    return celcius_value * 9.0 / 5 + 32
---
+++ chapter 10/roboadvisor/__main__.py
@@ -0,0 +1,7 @@
+
+from analysis.series import series_test
+from crawling.parser import parser-test
+
+if __name__ == '__main__':
+    series_test()
+    parser_test()
